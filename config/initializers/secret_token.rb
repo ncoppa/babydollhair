c85f61d839dc535f3bac0752c1704cd03ef395a7
Nicole Coppa <coppa.nicole@gmail.com>
2012-10-09 10:36:03 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Babydollhair::Application.config.secret_token = 'ee212c7d4e4e74cd8b269eb67926e0ded4ac6e85cdf396e4b165860ab3607b13c784ddc10dcd1e9f24513118f5c803a3d0869741a3bd3d5f6a088d27429f9f4f'
