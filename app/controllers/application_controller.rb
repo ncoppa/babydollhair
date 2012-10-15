c6b724d5cbac320e7478c03371a80fdb708b3b20
Nicole Coppa <coppa.nicole@gmail.com>
2012-10-15 13:53:14 -0700

    fixed herku error
    created a seed file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :load_nav
   def load_nav
-    @nav_links = Content.all
+    if Content.count > 0
+      @nav_links = Content.all      
+    end
   end
 end
